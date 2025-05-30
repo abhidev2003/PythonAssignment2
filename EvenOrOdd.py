94dd30e4be9285a3b9e9e06792dc929d899ecd23
Abhidev S P <abhidevsp2003@gmail.com>
2025-05-30 20:53:58 +0530

    Add files via upload

---
+++ EvenOrOdd.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+elif number % 2 == 1:
+    print(f"{number} is an odd number.")
+else:
+    print("What did you enter bro ?")
